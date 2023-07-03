f102045aa8055679bb4452bcbbf29001ce01b036
Ali <116851070+AeroAli@users.noreply.github.com>
2023-07-03 23:53:29 +0100

    modified bot to use db

--- cogs/hugs.py
+++ cogs/hugs.py
@@ -1,97 +1,83 @@
 # cogs / hugs.py
-import random
 import csv
+import random
 
 import discord
 from discord import Embed
 from discord.ext import commands
 
 
-class Hug(commands.Cog):
+class Affection(commands.Cog):
+    """Affectionate commands"""
     def __init__(self, client):
         self.client = client
-    
-    with open("gifs/hug_cog.csv", "r") as f:
-        # rows = csv.DictReader(f)
-        csv_rows = csv.reader(f, delimiter=",")
-        rows = list(csv_rows)
-        # print(type(rows))
-        boops = [row for row in rows if row[1] == 'boop']
-        cuddles = [row for row in rows if row[1] == 'cuddle']
-        hugs = [row for row in rows if row[1] == 'hug']
-        nohornys = [row for row in rows if row[1] == 'nohorny']
-        pats = [row for row in rows if row[1] == 'pat']
-        scritch = [row for row in rows if row[1] == 'scritch']
-        noms = [row for row in rows if row[1] == 'nom' or row[1] == "bite"]
-        tackles = [row for row in rows if row[1] == 'tackle']
-        squishes = [row for row in rows if row[1] == 'squish'] 
-    
 
-    
-    # Cuddle
-    @commands.hybrid_command()
-    # @commands.cooldown(1, 10, commands.BucketType.user)
-    async def bonk(self, ctx, user: discord.User):
-        chosen = random.choice(self.nohornys)
-        hug_choice = chosen[2]
-        message = [f"{ctx.author.display_name} bonks <@{user.id}>",  # author hugs @user
-                   f"{ctx.author.display_name} bonks {user.display_name}"]  # author hugs user
-        print(hug_choice)
-        embed_var = Embed(title="THWACK",description=random.choice(message))
-        embed_var.set_image(url=hug_choice)
+    hugging_table = "hug_cog"
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
+    async def cog_command_error(self, ctx, error: Exception) -> None:
+        print(error)
+    async def get_hugged(self, action):
+        async with self.client.db_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(
+                    f"SELECT * FROM `{self.hugging_table}` where category_name='{action}' ORDER BY RAND() LIMIT 1")
+                # print(cur.description)
+                result = await cur.fetchone()
+                print(result)
+                return result
 
 
     # Boop
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def boop(self, ctx, user: discord.User):
-        chosen = random.choice(self.boops)
+        """boop @user"""
+        chosen = await self.get_hugged("boop")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} boops <@{user.id}>",  # author hugs @user
                    f"{ctx.author.display_name} boops {user.display_name}"]  # author hugs user
         print(hug_choice)
-        embed_var = Embed(title="BOOPS",description=random.choice(message))
+        embed_var = Embed(title="BOOPS", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
-
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
     # Cuddle
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def cuddle(self, ctx, user: discord.User):
-        chosen = random.choice(self.cuddles)
+        """cuddle @user"""
+        chosen = await self.get_hugged("cuddle")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} cuddles <@{user.id}>",  # author hugs @user
                    f"{ctx.author.display_name} cuddles {user.display_name}"]  # author hugs user
         print(hug_choice)
-        embed_var = Embed(title="CUDDLES",description=random.choice(message))
+        embed_var = Embed(title="CUDDLES", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
     # Hug
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def hug(self, ctx, user: discord.User):
-        chosen = random.choice(self.hugs)
+        """hug @user"""
+        chosen = await self.get_hugged("hug")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} hugs <@{user.id}>",  # author hugs @user
                    f"{ctx.author.display_name} hugs {user.display_name}"]  # author hugs user
         print(hug_choice)
-        embed_var = Embed(title="HUGS",description=random.choice(message))
+        embed_var = Embed(title="HUGS", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
-
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
     # Tackle
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def tackle(self, ctx, user: discord.User):
-        chosen = random.choice(self.tackles)
+        """tackle @user"""
+        chosen = await self.get_hugged("tackle")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} tackles <@{user.id}>",  # author hugs @user3
                    f"{ctx.author.display_name} tackles {user.display_name}"]  # author hugs user
@@ -99,22 +85,15 @@ class Hug(commands.Cog):
         embed_var = Embed(title="TACKLE TIME", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
-
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if "hugbot" in message.content.lower() and "love" in message.content.lower():
-            # https://cdn.discordapp.com/emojis/1036757258006167703.gif?size=96&quality=lossless
-            emoji = self.client.get_emoji(1036757258006167703)
-            await message.add_reaction(emoji)
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
   
     # Scritches
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def scritches(self, ctx, user: discord.User):
-        chosen = random.choice(self.scritch)
+        """scritch @user"""
+        chosen = await self.get_hugged("scritch")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} scritches <@{user.id}>",  # author hugs @user3
                    f"{ctx.author.display_name} scritches {user.display_name}"]  # author hugs user
@@ -122,15 +101,14 @@ class Hug(commands.Cog):
         embed_var = Embed(title="SCRITCHES", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
-
-
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
     # Pat
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def pat(self, ctx, user: discord.User):
-        chosen = random.choice(self.pats)
+        """pat @user"""
+        chosen = await self.get_hugged("pat")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} pats <@{user.id}>",  # author hugs @user3
                    f"{ctx.author.display_name} pats {user.display_name}"]  # author hugs user
@@ -138,38 +116,60 @@ class Hug(commands.Cog):
         embed_var = Embed(title="*PATS*", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
 
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
-
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
     # Squish
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
     async def squish(self, ctx, user: discord.User):
-        chosen = random.choice(self.squishes)
+        """squish @user"""
+        chosen = await self.get_hugged("squish")
         hug_choice = chosen[2]
         message = [f"{ctx.author.display_name} squishes <@{user.id}>",  # author hugs @user3
                    f"{ctx.author.display_name} squishes {user.display_name}"]  # author hugs user
         print(hug_choice)
         embed_var = Embed(title="SQUISH", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
-        
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
 
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
 
-    # Noms
+    # Fav
     @commands.hybrid_command()
     # @commands.cooldown(1, 10, commands.BucketType.user)
-    async def nom(self, ctx, user: discord.User):
-        chosen = random.choice(self.noms)
+    async def glomp(self, ctx, user: discord.User):
+        """glomps @user"""
+        chosen = await self.get_hugged("favs")
         hug_choice = chosen[2]
-        message = [f"{ctx.author.display_name} noms <@{user.id}>",  # author hugs @user3
-                   f"{ctx.author.display_name} noms {user.display_name}"]  # author hugs user
+        message = [f"{ctx.author.display_name} attacks (affectionate) <@{user.id}>",  # author hugs @user3
+                   f"{ctx.author.display_name} attacks (affectionate) {user.display_name}"]  # author hugs user
         print(hug_choice)
-        embed_var = Embed(title="NOM!", description=random.choice(message))
+        embed_var = Embed(title="HUG!!!!!!", description=random.choice(message))
         embed_var.set_image(url=hug_choice)
-        
-        await ctx.reply(f"<@{user.id}>",embed=embed_var)
 
+        await ctx.reply(f"<@{user.id}>", embed=embed_var)
+
+    # Poke
+    @commands.hybrid_command()
+    # @commands.cooldown(1, 10, commands.BucketType.user)
+    async def poke(self, ctx, user: discord.User):
+        """pokes"""
+        embed_var = Embed(title=f"poke {user.display_name}")
+        # embed_var.set_image(url="https://media.tenor.com/9bPsSkaKgVsAAAAC/poke-gif")
+        embed_var.set_image(url="https://cdn.weeb.sh/images/rktSlkKvb.gif")
+        await ctx.reply(f"<@!{user.id}>", embed=embed_var)
+
+    # Happy Bot
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "hugbot" in message.content.lower() and "love" in message.content.lower():
+            # https://cdn.discordapp.com/emojis/1036757258006167703.gif?size=96&quality=lossless
+            emoji = self.client.get_emoji(1036757258006167703)
+            await message.add_reaction(emoji)
+
+    # eldritch
+    # flirt
+    # MemeHug
+    # bOnk!
 
 async def setup(client):
-    await client.add_cog(Hug(client))
+    await client.add_cog(Affection(client))
--- main.py
+++ main.py
@@ -1,31 +1,55 @@
 # main.py
-import discord
-from discord.ext import commands
+import asyncio
 from os import getenv, listdir
-from dotenv import load_dotenv
 from os.path import abspath, dirname
+
+import aiomysql
+import discord
+from aiohttp import ClientSession
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 token = getenv("TOKEN")
+db_pwd = getenv("DB_PWD")
+db_user = getenv("DB_USER")
+db_host = getenv("DB_HOST")
+db_port = getenv("DB_PORT")
+db = getenv("DB")
 
 
 class Client(commands.Bot):
-    def __init__(self):
+    def __init__(
+            self,
+            db_pool: aiomysql.Pool,
+            web_client: ClientSession,
+    ):
+
         super().__init__(
-            command_prefix=commands.when_mentioned_or("&"),
+            command_prefix=commands.when_mentioned_or("?"),
             intents=discord.Intents.all(),
             help_command=commands.DefaultHelpCommand(dm_help=True)
+
         )
+        self.db_pool = db_pool
+        self.web_client = web_client
 
     async def setup_hook(self):  # overwriting a handler
-        print(f"\033[31mLogged in as {client.user}\033[39m")
+        print(f"\033[31mLogged in as {self.user}\033[39m")
         cogs_folder = f"{abspath(dirname(__file__))}/cogs"
         for filename in listdir(cogs_folder):
             if filename.endswith(".py"):
-                await client.load_extension(f"cogs.{filename[:-3]}")
-        await client.tree.sync()
+                await self.load_extension(f"cogs.{filename[:-3]}")
+        await self.tree.sync()
         print("Loaded cogs")
 
 
-client = Client()
-client.run(token)
+async def main():
+    async with ClientSession() as session, aiomysql.create_pool(host=db_host, port=int(db_port),
+                                                                user=db_user, password=db_pwd, db=db, ) as pool:
+        client = Client(web_client=session, db_pool=pool)
+        await client.start(token)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
